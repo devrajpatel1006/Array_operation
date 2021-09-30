ac80d5c328d2bc3bacbb87616d169b75d6dd35ff
niketan-byte <54742264+niketan-byte@users.noreply.github.com>
2021-09-30 20:52:21 +0530

    Update MergeSort.cpp

--- Merge sort.cpp
+++ Merge sort.cpp
@@ -64,3 +64,28 @@ for(int k=0;k<n;k++)
 cout<<B[k]<<" ";
 	return 0;
 }
+int main()
+{ int n,i,j,lb,ub,k,mid;
+cout<<"Enter the size of array"<<"\n";
+cin>>n;
+int A[n],B[n];
+cout<<"Enter the elements of array"<<"\n";
+for(int t=0;t<n;t++)
+cin>>A[t];
+mergesort(A,B,0,n-1);
+//mergesort(A,B,mid+1,n-1);
+//merge(A,B,lb,mid,ub);
+for(int k=0;k<n;k++)
+cout<<B[k]<<" ";
+	return 0;
+void mergesort(int A[],int B[],int lb,int ub)
+{
+	if(lb<ub)
+	{
+		int mid = (lb+ub)/2;
+		
+		mergesort(A,B,lb,mid);
+		mergesort(A,B,mid+1,ub);
+		merge(A,B,lb,mid,ub);
+	}
+} }
