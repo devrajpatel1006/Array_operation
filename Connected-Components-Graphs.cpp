155b45e348804900594a10a1609ec7d44de6241e
Niketan0109 <56088377+Niketan0109@users.noreply.github.com>
2021-10-01 00:42:34 +0530

    Add Data Structure And Algorithm Related files

---
+++ Connected-Components-Graphs.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<bool> vis;
+int n,m;
+vector<vector<int>> adj;
+vector<int> components;
+
+int get_comp(int idx)
+{
+    if(vis[idx])
+    {
+        return 0;
+    }
+    vis[idx]=true;
+    int ans=1;
+    for(auto i : adj[idx])
+    {
+        if(!vis[i])
+        {
+            ans+=get_comp(i);
+            vis[i]=true;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    cin>>n>>m;
+    adj = vector<vector<int>>(n);
+    vis = vector<bool>(n,0);
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(!vis[i])
+        {
+            components.push_back(get_comp(i));
+
+        }
+    }
+    for(auto i : components)
+    {
+        cout<<i<<" ";
+    }
+
+    // choosing friend pair from other group
+
+    long long ans=0;
+    for(auto i : components)
+    {
+      ans+=i*(n-i);
+    }
+    //overcounting ko hatane ke liye
+    // 2 3
+    // 3 2 both are counted so divided by 2
+    cout<<"\n"<<(ans)/2;
+
+    return 0;
+}
